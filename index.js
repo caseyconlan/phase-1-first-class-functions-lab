95cff4f38124f0f4022127477575a3d715941d68
root <root@DESKTOP-1IHRCNC>
2023-03-21 15:29:43 -0700

    13 passing tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const driverList =['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return driverList.slice(0,2);
+}
+const returnLastTwoDrivers = function(drivers) {
+    return driverList.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+const createFareMultiplier = function(fareMultiplier) {
+    return function(number) {
+        return fareMultiplier * number;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driverList, arg) {
+    return arg(driverList);
+}
